4fc71c6d2020c2687e97fc81caa2a99293dde09e
DevOps <techtweek.com>
2023-04-13 15:19:30 +0530

    code added

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -10,7 +10,7 @@ resource "aws_lambda_function" "devon_lambda" {
   filename      = "${data.archive_file.lambda_package.output_path}"
   function_name = "devon_seacrest"
   handler       = "index.handler"
-  runtime       = "nodejs6.10"
+  runtime       = "nodejs14.x"
   role          = aws_iam_role.lambda_exec.arn
   source_code_hash = "${data.archive_file.lambda_package.output_base64sha256}"
 
